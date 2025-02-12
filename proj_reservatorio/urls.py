02554cb29e0980cffe6f14440a4e4aea595808fd
clara <claraavelino19@gmail.com>
2025-02-11 22:56:03 -0300

    Tela de login criada e estilizada com a API do Whatsap

--- proj_reservatorio/urls.py
+++ proj_reservatorio/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
+    path('login/', login, name="login"),
 ]
